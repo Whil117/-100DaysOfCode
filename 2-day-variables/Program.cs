903685730a777ce0094fa79765760a8ab7142bef
Milton Ivan (WhiL) García López <67771688+Whil117@users.noreply.github.com>
2023-06-30 20:03:15 -0600

    update day 3

--- 2-day-variables/Program.cs
+++ 2-day-variables/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿const string name = "WhiL";
+int age = 21, age2 = 22, age3 = 23;
+double hour = 7.29;
+bool isToday = true;
+Console.WriteLine($"Name :{name} \nAge: {age} {age2} {age3} \nHour: {hour}\nToday: {isToday}");
---
+++ 3-day-datatypes/Program.cs
@@ -0,0 +1,17 @@
+﻿int myNumb = 1223;
+long myNum = 15000000000L;
+float f1 = 35e3F;
+double d1 = 12E4D;
+const string numberInText = "123";
+
+
+Console.WriteLine(f1);
+Console.WriteLine(d1);
+Console.WriteLine(myNum);
+
+Console.WriteLine(myNumb);
+Console.WriteLine(Convert.ToString(myNumb) + " " + "Hello");
+
+Console.WriteLine(Convert.ToBoolean(myNum) + " " +"My Booolean");
+
+Console.WriteLine(Convert.ToString(numberInText) + " " +"My Convert Num to String");
