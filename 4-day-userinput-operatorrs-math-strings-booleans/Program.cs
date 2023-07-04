f935392d48261dbad9696a78650c1918fcfd05f9
Milton Ivan (WhiL) García López <67771688+Whil117@users.noreply.github.com>
2023-07-03 18:44:28 -0600

    New day, learned ReadLine and OperatorsNullEmpty with DateTime

---
+++ 4-day-userinput-operatorrs-math-strings-booleans/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Enter your name:");
+
+string myName = Console.ReadLine() ?? "default value";
+
+Console.WriteLine("Enter your age:");
+
+string getAge = Console.ReadLine() ?? string.Empty;
+
+bool validateAge = String.IsNullOrEmpty(getAge);
+
+int year = DateTime.Now.Year;
+
+if (validateAge)
+{
+ Console.WriteLine("Please Write your age in number");
+return;
+}
+
+int myAge = Convert.ToInt32(year) - Convert.ToInt32(getAge);
+
+
+Console.WriteLine($"Your name has: {myName.Length} characters");
+
+Console.WriteLine($"Your name is {myName} and you were bornin {myAge}");
