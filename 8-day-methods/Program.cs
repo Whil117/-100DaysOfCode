b7c0574719c39a8b3e2b7e65a583415d63ea6e48
Milton Ivan (WhiL) García López <67771688+Whil117@users.noreply.github.com>
2023-07-14 21:20:54 -0600

    update day 4 class

---
+++ 8-day-methods/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    string color = "red";
+    static void printYourName(string myName , string myLastName, int age )
+    {
+        Console.WriteLine($"Hello {myName} {myLastName} {age}");
+    }
+    static int sumNumbers(int numberOne, int numberTwo)
+    {
+        return numberOne + numberTwo;
+    }
+
+    static int myIntNumber(int x, int y)
+    {
+        return x + y;
+    }
+    static double myDoubleNumber(double x , double y)
+    {
+        return x + y;
+    }
+    static void Main()
+    {
+
+        Program myObj = new Program();
+        Console.WriteLine(myObj.color);
+    }
+}
