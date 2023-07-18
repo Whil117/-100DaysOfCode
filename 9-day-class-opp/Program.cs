062379978fa67b88f25b613051a82f273b18fba8
Milton Ivan (WhiL) García López <67771688+Whil117@users.noreply.github.com>
2023-07-17 21:12:58 -0600

    update day 9 c# class name methods

---
+++ 9-day-class-opp/Program.cs
@@ -0,0 +1,26 @@
+﻿class Food
+{
+    string name = "Hamburger";
+    string ingredient = "Sauce";
+    int price = 10;
+
+    public void  addPrice(int foodPrice)
+    {
+        price = foodPrice;
+
+    }
+    public void addIngredient (string newIngredient)
+    {
+       ingredient = newIngredient;
+        addPrice(newIngredient.Length);
+
+    }
+    static void Main(string[] args)
+    {
+        Food food  = new Food();
+        food.addIngredient("Ketchup");
+        Console.WriteLine(food.ingredient); //// Ketchup
+        Console.WriteLine(food.name); // Hamburger
+        Console.WriteLine(food.price); // ?????
+    }
+}
