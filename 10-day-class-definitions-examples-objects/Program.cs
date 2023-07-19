6f8a1d52f6ddad77b52975239ec8c7fc6149e00e
Milton Ivan (WhiL) García López <67771688+Whil117@users.noreply.github.com>
2023-07-18 22:53:10 -0600

    day 10 methods and access modifiers with classes constructors

---
+++ 10-day-class-definitions-examples-objects/Program.cs
@@ -0,0 +1,35 @@
+﻿
+class Car
+{
+    private string brand = "Tesla";
+    private int model = 2020;
+    private int price = 0;
+    public Car(string myBrand, int myModel)
+    {
+        brand = myBrand; // Set the initial value for model
+        model = myModel;
+    }
+    public void addPriceCar (int newPrice)
+    {
+        price = newPrice;
+    }
+    public string getCarBrand()
+    {
+        return brand;
+    }
+    public int getCarModel()
+    {
+        return model;
+    }
+
+}
+class Program
+{
+    static void Main()
+    {
+        Car car = new Car("Volvo", 2017);
+        Console.WriteLine(car.getCarBrand());
+        Console.WriteLine(car.getCarModel());
+     
+    }
+}
