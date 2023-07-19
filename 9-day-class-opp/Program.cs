cee2a2148fa071918c1d0793a75a48e1c38f4101
Milton Ivan (WhiL) García López <67771688+Whil117@users.noreply.github.com>
2023-07-18 21:12:11 -0600

    update class main with a different class Food

--- 9-day-class-opp/Program.cs
+++ 9-day-class-opp/Program.cs
@@ -1,25 +1,28 @@
 ﻿class Food
 {
-    string name = "Hamburger";
-    string ingredient = "Sauce";
-    int price = 10;
-    private string destination = "New york";
-
-
-    private void  addPrice(int foodPrice)
+    public string name = "Hamburger";
+    public string ingredient = "Sauce";
+    public int price = 10;
+    public string destination = "New york";
+    public void  addPrice(int foodPrice)
     {
         price = foodPrice;
-
     }
-    private void addIngredient (string newIngredient)
+    public void addIngredient (string newIngredient)
     {
        ingredient = newIngredient;
         addPrice(newIngredient.Length);
 
     }
-    static void Main(string[] args)
+}
+
+
+
+class Program
+{
+    static void Main(String[] args)
     {
-        Food food  = new Food();
+        Food food = new Food();
         Console.WriteLine(food.destination);
         food.addIngredient("Ketchup");
         Console.WriteLine(food.ingredient); //// Ketchup
