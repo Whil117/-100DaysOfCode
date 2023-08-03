ed320ee14bf2c9b8df510472d90c1864a7401a2e
Milton Ivan (WhiL) García López <67771688+Whil117@users.noreply.github.com>
2023-08-02 21:52:43 -0600

    class 11 access modifiers c#

---
+++ 11-accessmodifiers/Program.cs
@@ -0,0 +1,22 @@
+﻿
+//public accessible for all classes
+//private only it's accesible within the same class
+//protected accessible with the same class or inherited from that class.
+// intertal only assembly but not other assmebly
+
+class User
+{
+    private string name = "ivan";
+    public string lastName = "Garcia";
+     
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        User user = new User();
+        Console.WriteLine(user.lastName);
+        Console.WriteLine(user.name);
+    }
+}
