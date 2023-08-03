05d1e3caefaea8982054e591ecc2d62bc3aa41c3
Milton Ivan (WhiL) García López <67771688+Whil117@users.noreply.github.com>
2023-08-03 17:07:35 -0600

    new class 12, property and encapsulation class

---
+++ 12-properties/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel;
+using System.Security.Cryptography;
+using System.Xml.Linq;
+
+class Person
+{
+    private int age;
+
+    public int ChangeAge
+    {
+        get { return age; }   
+        set {
+
+            if (value >= 18)
+            {
+                age = value;
+            } else
+            {
+                age = 18;
+            }
+        }  
+    }
+}
+
+
+
+class Car
+{
+    private string status = "Your car is locked.";
+    private const string CorrectKey = "mykey";
+   
+    public string PutYourKeyCar(string key)
+    {
+        if (key == CorrectKey)
+        {
+            status = "Your car is open";
+            return "Correct password";
+        }else
+        {
+            return "Wrong password";
+        }
+    }
+    public string StatusCar
+    { get { return status; } }
+
+}
+class Program
+{
+    static void Main()
+    {
+        Car car = new Car();
+
+        string password =   car.PutYourKeyCar("123123");
+        Console.WriteLine(password);
+        Console.WriteLine(car.StatusCar);
+    }
+}
