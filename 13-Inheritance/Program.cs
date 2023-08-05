b4bb14f056245ede3a214bb914dfb1eb013fbcc4
Milton Ivan (WhiL) García López <67771688+Whil117@users.noreply.github.com>
2023-08-04 22:24:59 -0600

    update day 13 using inheritance

---
+++ 13-Inheritance/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Drawing;
+ class Car
+{
+    private string brand = "Tesla";
+    private int year = 2023;
+    private string color = "black";
+    public string ChangeColor
+    {
+        get { return color; }
+        set { color = value; }
+    }
+    public string GetColor()
+    {
+        return color;
+    }
+    public string GetBrand()
+    {
+        return brand;
+    }
+    public int GetYear()
+    {
+        return year;
+    }
+}
+
+class CompetitionCars:Car
+{
+    private string datenow = DateTime.Now.ToString();
+    public string GetNowDate()
+    {
+        return datenow;
+    }
+
+}
+class Program
+{
+    static void Main()
+    {
+        CompetitionCars competition = new CompetitionCars();
+        competition.ChangeColor = "red";
+        Console.Write(competition.GetColor());
+        Console.WriteLine(competition.GetNowDate());
+    }
+}
